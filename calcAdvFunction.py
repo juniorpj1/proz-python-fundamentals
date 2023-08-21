dbdda61cff0a63b762cdaca32bd7625e9ed15f4e
Aparicio Junior <apariciojunior11@gmail.com>
2023-08-21 15:54:13 -0300

    advanced calc function

---
+++ calcAdvFunction.py
@@ -0,0 +1,53 @@
+# Faça uma função calculadora que os números e as operações serão feitas pelo usuário. O código deve ficar rodando infinitamente até que o usuário escolha a opção de sair. No início, o programa mostrará a seguinte lista de operações:
+
+# 1: Soma
+# 2: Subtração
+# 3: Multiplicação
+# 4: Divisão
+# 0: Sair
+
+# Digite o número para a operação correspondente e caso o usuário introduza qualquer outro, o sistema deve mostrar a mensagem “Essa opção não existe” e voltar ao menu de opções.
+
+# Após a seleção, o sistema deve pedir para o usuário inserir o primeiro e segundo valor, um de cada. Depois precisa executar a operação e mostrar o resultado na tela. Quando o usuário escolher a opção “Sair”, o sistema irá parar.
+
+# É necessário que o sistema mostre as opções sempre que finalizar uma operação e mostrar o resultado.
+
+# Solução:
+
+def calculadora(num1, num2, operacao):
+    if operacao == 1:
+        return num1 + num2
+    elif operacao == 2:
+        return num1 - num2
+    elif operacao == 3:
+        return num1 * num2
+    elif operacao == 4:
+        return num1 / num2
+    else:
+        return 0
+
+
+while True:
+    print("1: Soma")
+    print("2: Subtração")
+    print("3: Multiplicação")
+    print("4: Divisão")
+    print("0: Sair")
+
+    operacao = int(input("Digite o número para a operação correspondente: "))
+
+    if operacao == 0:
+        break
+
+    num1 = int(input("Digite o primeiro valor: "))
+    num2 = int(input("Digite o segundo valor: "))
+
+    resultado = calculadora(num1, num2, operacao)
+
+    print(resultado)
+
+    print(" ")
+
+    print(" ")
+
+    print(" ")
